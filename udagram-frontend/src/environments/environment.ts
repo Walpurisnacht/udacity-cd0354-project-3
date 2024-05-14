892a75b70b47ddb77c0c3ad0b76e010721868c01
Walpurgisnacht Klarissa <walpurisnacht.klarissa@gmail.com>
2024-05-14 22:32:57 +0900

    Update reverse proxy environment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ade2b32f45ac04b8b98a43bcac262c28-2056622457.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
